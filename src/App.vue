915806a9647acde99adb9d726d7c6fdf369cabc3
PFilippov1 <p.filippov2.20@gmail.com>
2025-11-04 03:06:55 +0300

    test: husky with vue/js files

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
   <h1 class="text-3xl font-bold mb-2 flex justify-center">Vue Sneakers</h1>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-console.log('Hello there!')
-</script>
+<script setup></script>
 
 <style scoped></style>
